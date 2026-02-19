3773245696bfee7d0dcbf3fe8273e10156a74d1a
nicopignatto <81388990+nicopignatto@users.noreply.github.com>
2026-02-18 23:39:42 -0300

    Práctique Persistencia de Datos entre Escenas y Sesiones

--- Assets/Scripts Propios/MainManager.cs
+++ Assets/Scripts Propios/MainManager.cs
@@ -1,5 +1,8 @@
+
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.IO;
 using UnityEngine;
 
 public class MainManager : MonoBehaviour
@@ -19,5 +22,37 @@ public class MainManager : MonoBehaviour
 
         Instance = this;
         DontDestroyOnLoad(gameObject);
+        LoadColor();
     }
+
+    [Serializable]
+    public class SaveData
+    {
+        public Color teamColor;
+    }
+
+    public void SaveColor()
+    {
+        SaveData data = new SaveData();
+        data.teamColor = teamColor;
+
+        string json=JsonUtility.ToJson(data);
+        File.WriteAllText(Application.persistentDataPath + "/SavePlayerFile.json", json);
+
+    }
+
+    public void LoadColor()
+    {
+        string path = Application.persistentDataPath + "/SavePlayerFile.json";
+
+        if (File.Exists(path))
+        {
+            string json = File.ReadAllText(path);
+
+            SaveData data = JsonUtility.FromJson<SaveData>(json);
+
+            teamColor = data.teamColor;
+        }
+    }
+
 }
--- Assets/Scripts/MenuUIHandler.cs
+++ Assets/Scripts/MenuUIHandler.cs
@@ -17,12 +17,13 @@ public class MenuUIHandler : MonoBehaviour
         // add code here to handle when a color is selected
         MainManager.Instance.teamColor = color;
     }
-    
+
     private void Start()
     {
         ColorPicker.Init();
         //this will call the NewColorSelected function when the color picker have a color button clicked.
         ColorPicker.onColorChanged += NewColorSelected;
+        ColorPicker.SelectColor(MainManager.Instance.teamColor);
     }
 
     public void LoadNewScene()
@@ -32,7 +33,19 @@ public class MenuUIHandler : MonoBehaviour
 
     public void QuitToTheApp()
     {
+        MainManager.Instance.SaveColor();
         Application.Quit();//esta linea solo se ejecuta cuando se tiene una build del juego.
-        print("Salgo del juego");
+        //print("Salgo del juego");
+    }
+
+    public void SaveColorClicked()
+    {
+        MainManager.Instance.SaveColor();
+    }
+
+    public void LoadColorClicked()
+    {
+        MainManager.Instance.LoadColor();
+        ColorPicker.SelectColor(MainManager.Instance.teamColor);
     }
 }
